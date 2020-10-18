c3e08ddb63f2f88b29f6fb24190621de3834c5d5
Vane Bony <ie714415@iteso.mx>
2020-10-18 15:27:25 -0500

    Init sistem

--- main.c
+++ main.c
@@ -6,14 +6,32 @@
 #include "MK66F18.h"
 #include "fsl_debug_console.h"
 /* TODO: insert other include files here. */
-
+#include "BMI160.h"
+#include "mahony.h"
+#include "freertos_uart.h"
+#include "freertos_i2c.h"
+#include "task.h"
+#include "FreeRTOS.h"
+#include "FreeRTOSConfig.h"
 /* TODO: insert other definitions and declarations here. */
+#define BAUDRATE 115200
+#define RX 16
+#define TX 17
 
+typedef struct
+{
+	uint32_t header;
+	float x;
+	float y;
+	float z;
+} comm_msg_t;
 /*
  * @brief   Application entry point.
  */
-int main(void) {
+void init_sistem(void *parameters);
 
+int main(void)
+{
   	/* Init board hardware. */
     BOARD_InitBootPins();
     BOARD_InitBootClocks();
@@ -23,7 +41,10 @@ int main(void) {
     BOARD_InitDebugConsole();
 #endif
 
-    PRINTF("Hello World\n");
+    xTaskCreate(init_sistem, "init_sistem", 110, NULL, 1, NULL);
+
+      vTaskStartScheduler();
+
 
     /* Force the counter to be placed into memory. */
     volatile static int i = 0 ;
@@ -35,4 +56,32 @@ int main(void) {
         __asm volatile ("nop");
     }
     return 0 ;
+}
+
+void init_sistem(void *parameters)
+{
+	freertos_uart_flag_t uart_succes = freertos_uart_fail;
+	freertos_i2c_flag_t bmi160_sucess = freertos_i2c_fail;
+	uint8_t msg[] = "UART and BMI160 configured\n";
+	uint8_t * data = msg[0];
+
+	freertos_uart_config_t config;
+	config.baudrate = BAUDRATE;
+	config.pin_mux = kPORT_MuxAlt3;
+	config.port = freertos_uart_portB;
+	config.rx_pin = RX;
+	config.tx_pin = TX;
+	config.uart_number = freertos_uart0;
+
+	uart_succes = freertos_uart_init(config);
+	bmi160_sucess = bmi160_init();
+	if((freertos_uart_sucess == uart_succes) && (freertos_i2c_sucess == bmi160_sucess))
+	{
+		while('\0' != &data)
+		{
+			freertos_uart_send(freertos_uart0, &data, 1);
+			data++;
+		}
+	}
+	vTaskSuspend(NULL);
 }
